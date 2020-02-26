0a0010bb4574077919226ddcdee5be44d0ab15ed
marencamille19 <ste18009@byui.edu>
2020-02-25 17:12:32 -0700

    Added QuoteDate

--- Models/Desk.cs
+++ Models/Desk.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,21 @@ namespace MegaDesk2.Models
 {
     public class Desk
     {
+        public enum DesktopMaterial
+        {
+            Oak,
+            Laminate,
+            Pine,
+            Rosewood,
+            Veneer
+        }
 
+        public int Width { get; set; }
+        public int Depth { get; set; }
+        public int NumDrawers { get; set; }
+        public string Shipping { get; set; }
+        public DesktopMaterial Material { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime QuoteDate { get; set; }
     }
 }
